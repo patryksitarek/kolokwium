0c7dfbd43a08061e30d7ade4a42d7f8b1b44838a
pszemek <xd@LOL.COM>
2020-01-16 08:43:51 +0100

    first commit, zad4&5 done

---
+++ zad6/build/CMakeFiles/LibTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibTest.dir/src/libtest.cpp.o"
+  "libLibTest.pdb"
+  "libLibTest.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zad6/build/CMakeFiles/LibTest.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libLibTest.a"
+)
